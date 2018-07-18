2735b9d8ea79ab87014144761afd166acc95276d
4ndys0n23 <andres.soto.h@outlook.com>
2018-07-17 20:19:00 -0500

    Segunda parte del ejercicio 4 IMC

--- clase_2/ejercicio4.rb
+++ clase_2/ejercicio4.rb
@@ -1,18 +1,44 @@
 
 
 
-ran=rand(10-1) + 1
+# ran=rand(10-1) + 1
 
-p "Ingrese el número a comparar:"
+# p "Ingrese el número a comparar:"
 
-usr_num=gets.chomp.to_i
+# usr_num=gets.chomp.to_i
 
-if usr_num==ran
+# if usr_num==ran
 
-    p "Ganaste! el número es #{ran}"
+#     p "Ganaste! el número es #{ran}"
+
+# else
+
+#     p "Perdiste! el número es #{ran}"
+
+# end
+
+p "Ingrese su peso: (Kg)"
+
+peso=gets.chomp.to_f
+
+p "Ingrese su estatura: (m)"
+
+estat=gets.chomp.to_f
+
+bmi=(peso/(estat**2))
+
+p "Tu indice de masa corporal es de: #{bmi}"
+
+if bmi<18.5
+
+    p "Tu peso es BAJO."
+
+elsif bmi<25
+
+    p "Tu peso es NORMAL."
 
 else
 
-    p "Perdiste! el número es #{ran}"
+    p "Tienes SOBREPESO"
 
 end
