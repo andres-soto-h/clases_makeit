d8add0b73eb07e867c921381ee00c5e016ab8ffa
4ndys0n23 <andres.soto.h@outlook.com>
2018-07-18 20:01:54 -0500

    Ejemplos hashes clase 3

---
+++ clase_3/ejemplos_hash.rb
@@ -0,0 +1,25 @@
+
+persona_nueva={}
+persona2={}
+
+persona2={"nombre"=>"Andrés","edad"=>26}
+
+p persona2["nombre"]
+
+persona2["peso"]=70
+
+p persona2
+
+
+persona2.each do |llave, valor|
+    puts "#{llave}: #{valor}"
+end
+
+
+persona_nueva={:nombre =>"Andrés",:apellido=>"Soto",:edad=>26}
+
+p persona_nueva
+
+persona_nueva={nombre: "Andrés",apellido:"Soto",edad:26}
+
+p persona_nueva
