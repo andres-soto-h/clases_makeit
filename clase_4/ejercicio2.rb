338ad397b5c83cbcfa557e78fd32de65fac8aab1
4ndys0n23 <andres.soto.h@outlook.com>
2018-07-19 20:52:54 -0500

    Archivos complementarios ejercicio 2 y completar el ejercicio

--- clase_4/ejercicio2.rb
+++ clase_4/ejercicio2.rb
@@ -12,5 +12,21 @@
 
 # p file_as_array2
 
+array_mayor=[]
+
+file_as_array=IO.readlines('datos_ej.txt')
+
+file_as_array.each do |num|
+    
+    if num.to_i > 10
+        array_mayor.push(num.chomp)
+    end
+    
+end
+
+p array_mayor
+
+
+
 
 
