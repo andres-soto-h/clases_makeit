f29dcda13311bbb4da5768bb6e4e60c8482460da
4ndys0n23 <andres.soto.h@outlook.com>
2018-07-24 20:23:08 -0500

    Agrego los metodos del telefono al ejercicio del telefono

--- clase_5/ejercicio2.rb
+++ clase_5/ejercicio2.rb
@@ -1,4 +1,10 @@
 
+@directory={}
+
+
+
+
+
 class Telefono
 
     attr_accessor :calling, :contact, :phone
@@ -68,37 +74,87 @@ class Telefono
     def call_history
         p @call_log
     end
+
+    ##################  METODOS DE LA CLASE TELEFONO #####################
+
+    def import_directory(file_name)
+
+        n_contacts=0
+    
+        file_as_array=IO.readlines(file_name)
+    
+        file_as_array.each do |contact|
+            
+            dato_split=contact.split(";")
+            @directory[dato_split[0]]=dato_split[1]
+            n_contacts+=1
+    
+        end
+    
+        puts "--------------------------------------------------------------\n"+ 
+        "Se importaron #{n_contacts} contactos desde el directorio local.\n"+
+        "--------------------------------------------------------------\n"
+    end
+    
+    def list_contacts(directory_name)
+        directory_name.each_with_index do |(key,value),index|
+            p "#{index}: Usuario:#{key} Teléfono: #{value.chop}"
+        end
+    end
+    
+    def create_contact(user_name,phone,directory_name,file_name)
+        if !directory_name.has_key?(user_name)
+            
+            directory_name[user_name]=phone
+            File.open(file_name,'a'){|file| file.puts("#{user_name};#{phone}")}
+        
+        else
+            puts "Ya existe un usuario llamado: #{user_name} en el directorio.\n"
+        end
+    end
+    
+    def list_user_phone(user_name,directory_name)
+        if directory_name.has_key?(user_name)
+            puts "El teléfono es: #{directory_name[user_name]} \n"
+        else
+            puts "Lo sentimos, la persona que buscas no existe.\n"
+        end
+    end
+    
+    
 end
 
-l1=Telefono.new
+import_directory('..\clase_4\directorio.csv')
 
-l1.call("Pedro",198292)
+# l1=Telefono.new
 
-sleep(3)
+# l1.call("Pedro",198292)
 
-l1.show_call_info
+# sleep(3)
 
-l1.end_call
+# l1.show_call_info
 
-l1.call("Jesus",8888)
+# l1.end_call
 
-sleep(10)
+# l1.call("Jesus",8888)
 
-l1.end_call
+# sleep(10)
 
-l2=Telefono.new
+# l1.end_call
 
-l2.call("Juan",3983832)
+# l2=Telefono.new
 
-l2.show_call_info
+# l2.call("Juan",3983832)
 
-sleep(5)
+# l2.show_call_info
 
-l2.end_call
+# sleep(5)
 
-l1.call_history
+# l2.end_call
 
-l2.call_history
+# l1.call_history
 
-Telefono.get_devices
-Telefono.get_all_times
+# l2.call_history
+
+# Telefono.get_devices
+# Telefono.get_all_times
