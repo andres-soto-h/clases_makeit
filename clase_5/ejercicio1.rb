9dd4fc97e220950d4d518f5c900e3d21668f93a6
4ndys0n23 <andres.soto.h@outlook.com>
2018-07-26 23:36:56 -0500

    cambios para probar instancia de clases con modulo

--- clase_5/ejercicio1.rb
+++ clase_5/ejercicio1.rb
@@ -1,6 +1,89 @@
 require 'pry'
 
+
+module Conexiones
+
+    class GPS
+
+        attr_reader :lat_lng
+
+        def initialize()
+            @lat_lng=[]
+        end
+
+        def location    
+            lat_lng.push(rand(-90.0000000..90.0000000))
+            lat_lng.push(rand(-180.0000000..180.0000000))
+        end
+
+    end
+
+    class Internet
+
+        def speed
+            p "La velocidad de conexión es: #{rand(5.00..20.00).round(2)} MB"
+        end
+
+    end
+
+    class TV
+
+        attr_accessor :channels
+
+        def initialize(channel_list)
+            @channels=channel_list
+        end
+
+        def movie_on_channel(channel_name)
+            if @channels.has_key?(channel_name) 
+            
+                list=@channels[channel_name]
+                n=rand(0..list.length-1)
+                p "Estamos presentando: #{list[n]} en #{channel_name.to_s.upcase}"
+
+            else
+
+                p "No tienes #{channel_name.to_s.upcase} en tu paquete de TV."
+            
+            end
+        end
+
+    end
+
+
+    class Climate
+
+        def get_forecast(day_time)
+
+            try=0
+            forecast_result=0
+
+            options={soleado: Array(7..10), lluvia: Array(11..12), nublado: Array(13..16)}
+
+            options.each do |key,value|
+
+            if value.include?(day_time)  
+                    p "El pronóstico para las #{day_time} es: #{key.to_s}"
+                else 
+                    try+=1
+                end
+            
+            end
+
+            if try==3
+                 p "No hay pronosticos para las #{day_time}"
+            end
+
+        end
+
+    end
+    
+end
+
+
 class Bus
+
+    include Conexiones
 
     attr_accessor :speed, :passengers, :seating
 
@@ -105,85 +188,6 @@ class SuperBus < Bus
 end
 
 
-module Conexiones
-
-    class GPS
-
-        attr_reader :lat_lng
-
-        def initialize()
-            @lat_lng=[]
-        end
-
-        def location    
-            lat_lng.push(rand(-90.0000000..90.0000000))
-            lat_lng.push(rand(-180.0000000..180.0000000))
-        end
-
-    end
-
-    class Internet
-
-        def speed
-            p "La velocidad de conexión es: #{rand(5.00..20.00).round(2)} MB"
-        end
-
-    end
-
-    class TV
-
-        attr_accessor :channels
-
-        def initialize(channel_list)
-            @channels=channel_list
-        end
-
-        def movie_on_channel(channel_name)
-            if @channels.has_key?(channel_name) 
-            
-                list=@channels[channel_name]
-                n=rand(0..list.length-1)
-                p "Estamos presentando: #{list[n]} en #{channel_name.to_s.upcase}"
-
-            else
-
-                p "No tienes #{channel_name.to_s.upcase} en tu paquete de TV."
-            
-            end
-        end
-
-    end
-
-
-    class Climate
-
-        def get_forecast(day_time)
-
-            try=0
-            forecast_result=0
-
-            options={soleado: Array(7..10), lluvia: Array(11..12), nublado: Array(13..16)}
-
-            options.each do |key,value|
-
-            if value.include?(day_time)  
-                    p "El pronóstico para las #{day_time} es: #{key.to_s}"
-                else 
-                    try+=1
-                end
-            
-            end
-
-            if try==3
-                 p "No hay pronosticos para las #{day_time}"
-            end
-
-        end
-
-    end
-    
-end
-
 puts "--------------------------------------------------------------\n" +
 "                      SISTEMAS ADICIONALES DEL BUS                 \n" +
 "--------------------------------------------------------------" 
@@ -214,15 +218,19 @@ for1.get_forecast(14)
 for1.get_forecast(22)
 
 
-# p "-------------RESULTADOS RUTAS---------------"
+p "-------------RESULTADOS RUTAS---------------"
 
-# b0=MicroBus.new(10,{ruta1: "6-9", ruta2: "10-12", ruta3: "13-17"})
-# b0.add_passengers(5)
-# p "Pasajeros: #{b0.passengers}" 
-# b0.add_passengers(6)
-# p "Pasajeros: #{b0.passengers}" 
-# b0.validate_route(10)
-# b0.validate_route(14)
+b00=Bus.new(10)
+b00.add_passengers(5)
+p "Pasajeros: #{b00.passengers}" 
+
+
+b0=MicroBus.new(10,{ruta1: "6-9", ruta2: "10-12", ruta3: "13-17"})
+b0.add_passengers(5)
+p "Pasajeros: #{b0.passengers}" 
+b0.add_passengers(6)
+p "Pasajeros: #{b0.passengers}" 
+b0.validate_route(10)
 
 # p "-------------RESULTADOS SUPER BUS 1---------------"
 
