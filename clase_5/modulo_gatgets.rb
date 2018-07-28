20aa7779c7ca222bb17199fec8469741afd4253b
4ndys0n23 <andres.soto.h@outlook.com>
2018-07-27 20:43:37 -0500

    finalizo la clase temperatura de los gatgets

--- clase_5/modulo_gatgets.rb
+++ clase_5/modulo_gatgets.rb
@@ -2,7 +2,6 @@ module Gatgests
 
     class Calculator
 
-
         def sum_ab(a,b)
             a+b
         end
@@ -14,6 +13,23 @@ module Gatgests
     end
 
     class Thermometer
+        
+        def get_temperature(day_time)
+
+            case day_time            
+                when 0..9
+                    p "La temperatura es #{rand(12..16)}° Centigrados."
+                when 10..12
+                    p "La temperatura es #{rand(22..30)} Centigrados."
+                else
+                    p "La temperatura es #{rand(17..20)} Centigrados."
+            end
+
+        end
+    
+    end
+
+    class NotePad
 
     end
 
@@ -22,5 +38,9 @@ end
 c1=Gatgests::Calculator.new
 
 p c1.sum_ab(1,2)
-
 p c1.sum_ab(3,2)
+
+
+t1=Gatgests::Thermometer.new
+t1.get_temperature(8)
+t1.get_temperature(12)
