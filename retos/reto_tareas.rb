b405e247a919975ac52f3c9a5b3e6d3c2662cab1
4ndys0n23 <andres.soto.h@outlook.com>
2018-07-28 15:26:18 -0500

    completo reto de tareas

--- retos/reto_tareas.rb
+++ retos/reto_tareas.rb
@@ -1,30 +1,45 @@
 def load_tasks(file_name)
 
+    task_array=[]
+
     def to_b(str)
         str=="true" ? true :false
     end
 
-    file_as_array=IO.readlines(file_name)
-    task_array=[]
-    
-    file_as_array.each do |element|
-        
-        task_hash={}
-        
-        split=element.split(',')
-        
-        task_hash[:id]=split[0].to_i
-        task_hash[:name]=split[1]
-        task_hash[:done]=to_b(split[2])
+    if File.file?(file_name)
 
-        task_array.push(task_hash)
+        file_as_array=IO.readlines(file_name)
+            
+        file_as_array.each do |element|
+            
+            task_hash={}
+            
+            split=element.split(',')
+            
+            task_hash[:id]=split[0].to_i
+            task_hash[:name]=split[1]
+            task_hash[:done]=to_b(split[2])
 
+            task_array.push(task_hash)
+
+        end
     end
+    
     task_array
-end
-
-def save_tasks()
 
 end
 
-p load_tasks('tareas.txt')
+def save_tasks(file_name,array)
+
+    array.each do |value|
+        
+        File.open(file_name,'a'){|file| file.puts("#{value[:id]}, #{value[:name]}, #{value[:done]}")}
+    
+    end
+
+end
+
+save_tasks('tareas2.txt', [
+    { id: 1, name: "Hacer tareas", done: true },
+    { id: 2, name: "Lavar ropa", done: false }
+  ])
