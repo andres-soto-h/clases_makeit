f2c1b5b41083633ba85f9089f4dc99665f208920
4ndys0n23 <andres.soto.h@outlook.com>
2018-07-30 19:54:48 -0500

    quito las ordenes del modulo de facturación

--- app_restaurante/checkout_module.rb
+++ app_restaurante/checkout_module.rb
@@ -1,15 +1,9 @@
 require 'fileutils'
-require 'pry'
-require_relative 'orders_module'
 
 module Payments
 
-    @@orders_aux=[]
-
     class RestaurantCheckout
 
-        include Orders
-        
         @@order_payments=[]
         @@total_income=0
 
@@ -17,44 +11,24 @@ module Payments
                         
         end
 
-        def order_pay
-            
-            puts "--------------------------------------------------------\n"
-                 "-------------------INICIA PROCESO DE PAGO---------------\n"
-                 "--------------------------------------------------------\n"
-
-            puts "\n\nIngrese el número de la mesa que desea facturar:"
-            table_num=gets.chomp.to_i
-            total_payment=RestaurantOrder.table_total(table_num)
-            puts "\n\n¿Desea proceder con el pago? (s) (n)\n\n"
-            ans=gets.chomp
-
-            if ans.upcase=="S"
-                @@orders_aux=RestaurantOrder.return_orders
-
-                @@orders_aux.each do |order|
-                    
-                    if order[:table]==table_num.to_i
+        def order_pay(ordenes)
+                ordenes.each do |order|    
+                if order[:table]==table_num.to_i
                         @@order_payments.push({payment_id: Time.now, table: table_num, detail: order[:detail]})
                     end
                 end
-    
-                RestaurantOrder.delete_order(table_num)
                 @@total_income+=total_payment
-        
             end
         end
 
         def get_payment
             @@order_payments.each_with_index do |payment, index|
-
                 puts "------------------------------------------------\n\n"
                 puts "#{index+1}. Id del pago: #{payment[:payment_id]} Mesa: #{payment[:table]} Productos:\n\n"
-            
+
                 payment[:detail].each do |payment_detail|
                     puts "#{payment_detail[:name]} --------- #{payment_detail[:price]}\n\n"
                 end
-
                 puts "------------------------------------------------\n\n"
             end
         end
