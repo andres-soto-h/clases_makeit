3de65226066e5e4a8569a398da9d7dc659c9092e
4ndys0n23 <andres.soto.h@outlook.com>
2018-07-30 19:13:27 -0500

    quitar lineas

--- app_restaurante/checkout_module.rb
+++ app_restaurante/checkout_module.rb
@@ -26,37 +26,29 @@ module Payments
             puts "\n\nIngrese el número de la mesa que desea facturar:"
             table_num=gets.chomp.to_i
             total_payment=RestaurantOrder.table_total(table_num)
-
             puts "\n\n¿Desea proceder con el pago? (s) (n)\n\n"
             ans=gets.chomp
 
             if ans.upcase=="S"
-
                 @@orders_aux=RestaurantOrder.return_orders
-
-                ##binding.pry
 
                 @@orders_aux.each do |order|
                     
                     if order[:table]==table_num.to_i
-    
                         @@order_payments.push({payment_id: Time.now, table: table_num, detail: order[:detail]})
-                        
                     end
                 end
     
                 RestaurantOrder.delete_order(table_num)
                 @@total_income+=total_payment
-            
+        
             end
         end
 
         def get_payment
-            
             @@order_payments.each_with_index do |payment, index|
 
                 puts "------------------------------------------------\n\n"
-
                 puts "#{index+1}. Id del pago: #{payment[:payment_id]} Mesa: #{payment[:table]} Productos:\n\n"
             
                 payment[:detail].each do |payment_detail|
@@ -64,16 +56,13 @@ module Payments
                 end
 
                 puts "------------------------------------------------\n\n"
-            
             end
         end
 
         def total_payments
-
             puts "----------------------------------------------------------\n\n"
             puts "La facturación total ha sido de: #{@@total_income}\n\n"
             puts "----------------------------------------------------------\n\n"
-        
         end
 
     end
