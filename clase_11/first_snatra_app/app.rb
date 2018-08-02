c8089951c419ebd46b837bf3909180ceeb359431
4ndys0n23 <andres.soto.h@outlook.com>
2018-08-01 20:42:15 -0500

    practica de metodos http

--- clase_11/first_snatra_app/app.rb
+++ clase_11/first_snatra_app/app.rb
@@ -1,25 +1,49 @@
 require 'bundler/setup'
 Bundler.require(:default)
 
-get '/' do
-    @name='Andres Soto'
-    erb :index
+# get '/' do
+#     @name='Andres Soto'
+#     erb :index
+# end
+
+# get '/home' do
+#     @saluda='Welcome to my first app'
+#     erb :home
+# end
+
+
+# get '/contact' do
+#     @correo='andres.soto.h@outlook.com'
+#     @telefono='3134123712'
+#     erb :contact
+# end
+
+
+# get '/products' do
+#     @products=['web design','backend development']
+#     erb :products
+# end
+
+
+# get '/saludo' do
+#     "Hola #{params[:name]} #{params[:tipo]}"
+# end
+
+
+get '/make-a-get' do
+    [200, {"Content-Type" => "Text/html"}, "Esto es un get"]
 end
 
-get '/home' do
-    @saluda='Welcome to my first app'
-    erb :home
+post '/make-a-post' do
+    [200, {"Content-Type" => "Text/html"}, "Esto es un post"]
+end
+
+put '/make-a-put' do
+    [200, {"Content-Type" => "Text/html"}, "Esto es un put"]
+end
+
+delete '/make-a-delete' do
+    [200, {"Content-Type" => "Text/html"}, "Esto es un delete"]
 end
 
 
-get '/contact' do
-    @correo='andres.soto.h@outlook.com'
-    @telefono='3134123712'
-    erb :contact
-end
-
-
-get '/products' do
-    @products=['web design','backend development']
-    erb :products
-end
