904860d26c885fe09d6385e49717df8bc6a09a64
4ndys0n23 <andres.soto.h@outlook.com>
2018-08-05 08:27:30 -0500

    espacios...

--- clase_12/app.rb
+++ clase_12/app.rb
@@ -1,80 +1,103 @@
 require 'bundler/setup'
 Bundler.require(:default)
+require './models.rb'
 
+set :database, 'sqlite3:myblogdb.sqlite3'
 
 get '/' do
-    #binding.pry
-    if request.cookies["email"] && request.cookies["password"]
-        @name="Andrés Soto"
-        erb :home
-    else
-        #binding.pry
-        redirect '/login'
-    end
+  erb :home, layout: :home_layout
+end
+
+post '/post' do
+  @post = Post.create(title: params[:title], body: params[:body])
+  redirect '/blog'
+end
+
+get '/post/:id' do
+  begin
+    @post = Post.find(params[:id])
+    erb :post_page
+  rescue StandardError => e
+    erb :error_page
+  end
+end
+
+put '/post/:id' do
+  @post = Post.find(params[:id])
+  @post.update(title: params[:title], body: params[:body])
+  @post.save
+  redirect '/post/' + params[:id]
+end
+
+delete '/post/:id' do
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect '/blog'
 end
 
 get '/login' do
-    erb :login
+  erb :login, layout: :layout_login
 end
 
 post '/login' do
-    response.set_cookie('email',params[:email])
-    response.set_cookie('password',params[:password])
-    redirect '/home'
+  response.set_cookie('email', params[:email])
+  response.set_cookie('password', params[:password])
+  redirect '/home'
 end
 
 post '/logout' do
-    session.clear
-    redirect 'login'
+  response.delete_cookie(:email, value = {})
+  response.delete_cookie(:password, value = {})
+  redirect 'login'
 end
 
-get '/registro' do    
-    erb :form
+get '/registro' do
+  erb :form
 end
-
 
 post '/register-results' do
-    @name=params[:name]
-    @last_name=params[:last_name]
-    @address=params[:address]
-    @email=params[:email]
-    @age=params[:age]
-    @sex=params[:sex]
-    erb :form_data
+  @name = params[:name]
+  @last_name = params[:last_name]
+  @address = params[:address]
+  @email = params[:email]
+  @age = params[:age]
+  @sex = params[:sex]
+  erb :form_data
 end
 
 get '/home' do
-    if request.cookies["email"] && request.cookies["password"]
-        @saluda='Welcome to my first app'
-        @name="Andrés Soto"
-        erb :home
-    else
-        #binding.pry
-        redirect '/login'
-    end
+  if request.cookies['email'] && request.cookies['password']
+    erb :home, layout: :home_layout
+  else
+    redirect '/login'
+  end
 end
 
 get '/contact' do
-
-    if request.cookies["email"] && request.cookies["password"]
-        @correo='andres.soto.h@outlook.com'
-        @telefono='3134123712'
-        erb :contact
-    else
-        #binding.pry
-        redirect '/login'
-    end
-
+  if request.cookies['email'] && request.cookies['password']
+    @correo = 'andres.soto.h@outlook.com'
+    @telefono = '3134123712'
+    erb :contact
+  else
+    redirect '/login'
+  end
 end
-
 
 get '/products' do
-    if request.cookies["email"] && request.cookies["password"]
-        @products=['web design','backend development']
-        erb :products
-    else
-        #binding.pry
-        redirect '/login'
-    end
+  if request.cookies['email'] && request.cookies['password']
+    @products = ['web design', 'backend development']
+    erb :products
+  else
+    redirect '/login'
+  end
 end
 
+get '/blog' do
+  if request.cookies['email'] && request.cookies['password']
+    @posts = Post.all
+    @name = 'Andrés Soto'
+    erb :blog, layout: :blog_layout
+  else
+    redirect '/login'
+  end
+end
