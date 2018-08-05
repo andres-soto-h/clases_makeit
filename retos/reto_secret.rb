b9f10b6c196e328826b2a1f92fa08b13e6d089a3
4ndys0n23 <andres.soto.h@outlook.com>
2018-08-05 12:01:17 -0500

    completo el reto

--- retos/reto_secret.rb
+++ retos/reto_secret.rb
@@ -1,12 +1,28 @@
+# def secret_code(phrase)
+#   words = phrase.split(' ')
+#   compare_arr = %w[one two three four five six seven eight nine]
+#   code = 0
+#   words.each do |word|
+#     compare_arr.each_with_index do |num, index|
+#       code += (index + 1) if word == num
+#     end
+#   end
+#   code
+# end
+
 def secret_code(phrase)
-    compare_arr=['one','two','three','four','five','six','seven','eight','nine']
-    code=0
-    compare_arr.each_with_index |num, index|    
-        if phrase.include?(num)
-            code+=1
-        end
-    end
-    code
+  numbers = { 'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5,
+              'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9 }
+
+  value = 0
+  phrase.split(/\W/).each do |word|
+    value += numbers[word] if numbers[word]
+  end
+
+  value
 end
 
-puts secret_code("one on one")
+puts secret_code('one on one') #=> 2
+puts secret_code('five monkeys in three farms') #=> 8
+puts secret_code('') #=> 0
+puts secret_code('fivethreeone') #=> 0
