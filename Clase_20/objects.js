87653e46e6c8ba0bd8716e4e5d5395924538677f
andres-soto-h <andres.soto.h@outlook.com>
2018-09-01 12:01:47 -0500

    ejemplo de herencia en js

--- Clase_20/objects.js
+++ Clase_20/objects.js
@@ -1,116 +1,130 @@
-// // // var personaLiteral={
-// // //     nombre: "Juan Literal",
-// // //     apellido: "Hz",
-// // //     saluda: function(){
-// // //         return "Hola soy " + this.nombre;
-// // //     }
-// // // };
-
-// // // var Persona = function(nombre, apellido){
-// // //     this.nombre=nombre;
-// // //     this.apellido=apellido;
-// // //     this.saluda=function(nombre2){
-// // //         console.log("hola "+nombre2+"soy "+this.nombre);
-// // //     };
-// // // };
-
-// // // p1=new Persona("Pepito", "Perez");
-
-// // // p1.saluda('Pedro');
-
-// // // function printObject(obj){
-// // //     for (var key in obj){
-// // //         console.log(key+": "+obj[key]);
-// // //     }
-// // // }
-
-// // // printObject(p1);
-
-
-// // var Car = function (brand, model, cc) {
-// //     this.brand = brand;
-// //     this.model = model;
-// //     this.cc = cc;
-// //     this.speed = 0;
-// //     this.accelerate = function () {
-// //         this.speed += (0.003 * this.cc);
-// //     };
-// //     this.break = function () {
-// //         new_speed = this.speed - (0.0035 * this.cc);
-// //         if (new_speed < 0) {
-// //             this.speed = 0;
-// //         } else {
-// //             this.speed = new_speed;
-// //         }
-// //     };
-// // }
-
-// // function compareSpeed(car1, car2) {
-// //     if (car1.speed > car2.speed) {
-// //         return car1.brand + " is Faster than " + car2.brand
-// //     } else {
-// //         return car2.brand + " is Faster than " + car1.brand
+// // var personaLiteral={
+// //     nombre: "Juan Literal",
+// //     apellido: "Hz",
+// //     saluda: function(){
+// //         return "Hola soy " + this.nombre;
 // //     }
 // // };
 
-// // car1 = new Car("Mercedez", "XZ", 5000);
-// // car1.accelerate();
-// // car1.accelerate();
+// var Persona = function(nombre, apellido){
+//     this.nombre=nombre;
+//     this.apellido=apellido;
+//     this.saluda=function(nombre2){
+//         console.log("hola "+nombre2+"soy "+this.nombre);
+//     };
+// };
 
-// // car2 = new Car("Lamborgini", "Murcielago", 8000);
-// // car2.accelerate();
-// // car2.accelerate();
+// // p1=new Persona("Pepito", "Perez");
 
-// // console.log(car1.speed);
-// // console.log(car2.speed);
+// // p1.saluda('Pedro');
 
-// // console.log(compareSpeed(car1, car2));
-
-
-// // //Recursividad
-
-// // var factorial = function (n) {
-// //     if (n == 0) {
-// //         return 1;
-// //     } else {
-// //         return n * factorial(n - 1);
+// // function printObject(obj){
+// //     for (var key in obj){
+// //         console.log(key+": "+obj[key]);
 // //     }
 // // }
 
-// // console.log(factorial(3));
+// // printObject(p1);
 
 
-// // //funcion revursiva para determinar si un numero es par o impar
+var Car = function (brand, model, cc) {
+    this.brand = brand;
+    this.model = model;
+    this.cc = cc;
+    this.speed = 0;
+    this.accelerate = function () {
+        this.speed += (0.003 * this.cc);
+    };
+    this.break = function () {
+        new_speed = this.speed - (0.0035 * this.cc);
+        if (new_speed < 0) {
+            this.speed = 0;
+        } else {
+            this.speed = new_speed;
+        }
+    };
+}
+
+function compareSpeed(car1, car2) {
+    if (car1.speed > car2.speed) {
+        return car1.brand + " is Faster than " + car2.brand
+    } else {
+        return car2.brand + " is Faster than " + car1.brand
+    }
+};
 
 
-// // function isEven(n) {
-// //     if (n == 0) {
-// //         return "Is Even"
-// //     } else if (n == 1) {
-// //         return "Is Odd"
-// //     } else {
-// //         return isEven(n - 2);
-// //     }
-// // }
+car1 = new Car("Mercedez", "XZ", 5000);
+car1.accelerate();
+car1.accelerate();
 
-// // console.log(isEven(7));
+Car.prototype.aceite="Full"
 
-// // //escribir una función recursiva que sume los elementos de un array
+car2 = new Car("Lamborgini", "Murcielago", 8000);
+car2.accelerate();
+car2.accelerate();
 
-// // var arraySum = function (my_array){
-// //     if (my_array.length===1){
-// //         return my_array[0];
-// //     }else{
-// //         return my_array.pop() + arraySum(my_array)
-// //     }
-// // }
+console.log(car1.speed);
+console.log(car2.speed);
 
-// // console.log(arraySum([1, 2, 3]));
+console.log(compareSpeed(car1, car2));
+console.log(car2.aceite)
+
+var Bus = function(marca, modelo, cc, capacidad){
+  Car.call(this, marca, modelo, cc);
+  this.capacidad=capacidad;
+}
+
+Bus.prototype = new Car();
+
+b1 = new Bus("Marcopolo", "Trail", 5000, 50);
+
+console.log(b1.model)
+
+// //Recursividad
+
+// var factorial = function (n) {
+//     if (n == 0) {
+//         return 1;
+//     } else {
+//         return n * factorial(n - 1);
+//     }
+// }
+
+// console.log(factorial(3));
+
+
+// //funcion revursiva para determinar si un numero es par o impar
+
+
+// function isEven(n) {
+//     if (n == 0) {
+//         return "Is Even"
+//     } else if (n == 1) {
+//         return "Is Odd"
+//     } else {
+//         return isEven(n - 2);
+//     }
+// }
+
+// console.log(isEven(7));
+
+// //escribir una función recursiva que sume los elementos de un array
+
+// var arraySum = function (my_array){
+//     if (my_array.length===1){
+//         return my_array[0];
+//     }else{
+//         return my_array.pop() + arraySum(my_array)
+//     }
+// }
+
+// console.log(arraySum([1, 2, 3]));
 
 
 
 
-// //edad y funcion crecer que cada vez que se llame aumente la edad en un año
+//edad y funcion crecer que cada vez que se llame aumente la edad en un año
 
 // var Persona = function(nombre, apellido){
 //     this.nombre=nombre;
