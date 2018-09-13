b80bcb303cfb0c17fcd47e3732a12a3ee1277cf0
andres-soto-h <andres.soto.h@outlook.com>
2018-09-13 18:22:05 -0500

    testing controller

--- rails_2/rails-modals/app/controllers/products_controller.rb
+++ rails_2/rails-modals/app/controllers/products_controller.rb
@@ -11,14 +11,17 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create(product_params)
+    respond_to do |format|
+      format.html { redirect_to products_path }
+    end
   end
 
   def edit
-    @product=Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
   def update
-    @product=Product.update(params[:id], product_params)
+    @product = Product.update(params[:id], product_params)
   end
 
   def destroy
@@ -27,6 +30,7 @@ class ProductsController < ApplicationController
   end
 
   private
+
   def product_params
     params.require(:product).permit(:name, :price)
   end
--- rails_2/rails-modals/test/controllers/products_controller_test.rb
+++ rails_2/rails-modals/test/controllers/products_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
+# rails test test/controllers/products_controller_test.rb
+
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get products_index_url
+  test 'get index: is successful' do
+    get products_path
     assert_response :success
   end
 
+  test 'post create: creates a product' do
+    assert_difference 'Product.count', 1 do
+      post products_path, params: { product: { name: 'product 1', price: 2, published: true, cost: 1.3, stock: 2 } }
+    end
+  end
 end
