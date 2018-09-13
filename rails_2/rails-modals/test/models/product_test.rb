e4e020a10c96c72c0d0d1ad5f5438321d47c4afa
andres-soto-h <andres.soto.h@outlook.com>
2018-09-12 20:05:57 -0500

    ruby test for models

--- rails_2/rails-modals/test/models/product_test.rb
+++ rails_2/rails-modals/test/models/product_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup; end
+
+  test 'Product is not created without a name' do
+    product = Product.new
+    assert_equal false, product.save
+  end
+
+  test '.word_count returns the correct number of words in name' do
+    product = Product.new(name: 'hola mundo. Esto es una prueba!')
+    assert_equal 6, product.word_count
+  end
+
+  test 'published scope only returns published Products' do
+    products = Product.published
+    assert_equal 2, products.length
+  end
+
+  test 'get product profit' do
+    product = Product.first
+    assert_equal '100.0%', product.profit
+  end
+
+  test 'check if product is available in stock' do
+    product = Product.last
+    assert_equal true, product.stock_status
+  end
 end
